98b252598bc850b722d39b169ef4d4f7e2f92ea2
Ivan Kopiev <kopiev@mustdev.ru>
2022-07-14 07:17:08 +0300

    Initial Commit

---
+++ VKGroup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKGroup
+//
+//  Created by Ivan Kopiev on 14.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
