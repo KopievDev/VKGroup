8fef3405fd1c54d887dfdaef50b8052ea5e5608f
Ivan Kopiev <kopiev@mustdev.ru>
2022-07-14 11:41:57 +0300

    Refactoring again

--- VKGroup/Extensions/UIImageView.swift
+++ VKGroup/Extensions/UIImageView.swift
@@ -14,29 +14,25 @@ extension UIImageView {
         guard let url = URL(string: urlString) else { return }
         let request = URLRequest(url: url)
         if cache.cachedResponse(for: request) != nil {
-            loadImageFromCache(imageURL: url)
+            loadImage(on: request, from: cache)
         } else {
-            downloadImage(imageURL: url)
+            downloadImage(on: request)
         }
     }
     
-    private func downloadImage(imageURL: URL) {
-        let request = URLRequest(url: imageURL)
+    private func downloadImage(on request: URLRequest) {
         URLSession.shared.dataTask(with: request) { data, _, error in
             guard let data = data, error == nil, let image = UIImage(data: data) else { return }
             DispatchQueue.main.async { self.image = image }
         }.resume()
     }
     
-    private func loadImageFromCache(imageURL: URL) {
-        let cache = URLCache.shared
-        let request = URLRequest(url: imageURL)
+    private func loadImage(on request: URLRequest, from cache:URLCache) {
         DispatchQueue.global(qos: .userInitiated).async {
             if let data = cache.cachedResponse(for: request)?.data, let image = UIImage(data: data) {
                 DispatchQueue.main.async { self.image = image }
             }
         }
     }
-    
 }
 
